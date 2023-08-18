1bdf8707dad78bb6aa961d5f61d14c82aedad088
PARTH RASTOGI <84573632+Parthcode1005@users.noreply.github.com>
2023-08-18 22:56:59 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+const inp = document.getElementById("inp-word");
+
+inp.addEventListener("change", () => {
+    let inpWord = document.getElementById("inp-word").value;
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="word">
+                    <h3>${inpWord}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>`;
+            sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
+        })
+        .catch(() => {
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
+});
+
+// Event listener for Enter key press on input field
+inp.addEventListener("keydown", (event) => {
+    if (event.key === 13) {
+        event.preventDefault();
+        const inpWord = inp.value;
+        fetchWordDetails(inpWord);
+    }
+});
+
+function playSound() {
+    sound.play();
+}
+
+// Add that javascript code in this code
